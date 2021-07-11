9c175b292b768cd3f659ef6ab30ef2487649acad
takuoka <blackn.red42@gmail.com>
2021-07-11 18:12:45 +0900

    Small Fix

--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,7 @@ const Footer = ({ fullWidth }) => {
       <div className="my-4 text-sm leading-6">
         <div className="flex align-baseline justify-between flex-wrap">
           <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}<Link href="/feed"><a style={{opacity: '0.0'}}>RSS</a></Link>
+            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`} <Link href="/feed"><a style={{opacity: '0.0'}} >RSS</a></Link>
           </p>
         </div>
       </div>
